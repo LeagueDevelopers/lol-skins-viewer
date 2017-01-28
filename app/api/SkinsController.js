400232c9a0eca7e57a4821fa43135bd9463bff41
Sergio Coimbra <sercoimbra@gmail.com>
2017-01-28 15:58:17 +0000

    Move network requests to Chromium stack (renderer)

--- app/api/SkinsController.js
+++ app/api/SkinsController.js
@@ -1,10 +1,9 @@
 // @flow
-import request from 'request-promise-native';
 import flatMap from 'lodash.flatmap';
 import type { Summoner, Champion } from '../types';
 
 function transformResult (champions): Champion[] {
-//  const ownedChampions = champions.filter(c => c.ownership.owned);
+  //  const ownedChampions = champions.filter(c => c.ownership.owned);
   return champions.reduce((acc, c) => {
     const champSkins = c.skins.filter(s => s.name !== c.name).map(s => ({
       id: s.id,
@@ -31,21 +30,18 @@ function transformResult (champions): Champion[] {
   }, []);
 }
 
-export function getData (proxyPort: number, summoner : Summoner) {
-  return request({
-    uri: `http://127.0.0.1:${proxyPort}/lol-collections/v1/inventories/${summoner.id}/champions`,
-    json: true
-  })
-    .then(async res => {
-      const champions = transformResult(res);
-      const ownedChampionIds = res.filter(c => c.ownership.owned).map(c => c.id);
-      const skinIds = flatMap(champions, c => c.skins).map(s => s.id);
-      const skinMetadata = {};
-      for (const id of skinIds) {
-        const meta = await request({
-          uri: `http://127.0.0.1:${proxyPort}/lol-loot/v1/player-loot/CHAMPION_SKIN_${id}`,
-          json: true
-        });
+export async function getData (proxyPort: number, summoner: Summoner) {
+  const collectionResponse = await fetch(`http://127.0.0.1:${proxyPort}/lol-collections/v1/inventories/${summoner.id}/champions`);
+  if (collectionResponse.ok) {
+    const collectionData = await collectionResponse.json();
+    const champions = transformResult(collectionData);
+    const ownedChampionIds = collectionData.filter(c => c.ownership.owned).map(c => c.id);
+    const skinIds = flatMap(champions, c => c.skins).map(s => s.id);
+    const skinMetadata = {};
+    for (const id of skinIds) {
+      const response = await fetch(`http://127.0.0.1:${proxyPort}/lol-loot/v1/player-loot/CHAMPION_SKIN_${id}`);
+      if (response.ok) {
+        const meta = await response.json();
         skinMetadata[id] = {
           id,
           lootId: meta.lootId,
@@ -55,10 +51,12 @@ export function getData (proxyPort: number, summoner : Summoner) {
           tags: Array.isArray(meta.tags) ? meta.tags : meta.tags.split(',')
         };
       }
-      return {
-        champions,
-        skinMetadata,
-        ownedChampionIds
-      };
-    });
+    }
+    return {
+      champions,
+      skinMetadata,
+      ownedChampionIds
+    };
+  }
+  return collectionResponse;
 }
